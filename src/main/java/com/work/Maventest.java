97225e80482c76a0fead2c8a9066ec621226f6d6
GirishNB1 <87060319+GirishNB1@users.noreply.github.com>
2025-10-03 15:26:49 +0530

    Initial commit

---
+++ src/main/java/com/work/Maventest.java
@@ -0,0 +1,10 @@
+package com.work;
+
+public class Maventest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("sgss "+System.getProperty("java.runtime.version"));
+	}
+
+}
---
+++ src/test/java/com/work/MaventestTest.java
@@ -0,0 +1,14 @@
+package com.work;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaventestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
