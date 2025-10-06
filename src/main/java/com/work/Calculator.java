389ea9524be6c8704faadd3282474553f99f00a8
GirishNB1 <87060319+GirishNB1@users.noreply.github.com>
2025-10-06 22:14:59 +0530

    commit

---
+++ src/main/java/com/work/Calculator.java
@@ -0,0 +1,25 @@
+package com.work;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println(add(5,4));
+//      System.out.println(sub(5,4));
+//      System.out.println(prod(5,4));
+	}
+	 static int add(int a,int b) {
+		return a+b;
+	}
+	
+	 static int sub(int a,int b) {
+		return a-b;
+	}
+	
+	static int prod(int a,int b) {
+		return a*b;
+	}
+
+}
+
+
