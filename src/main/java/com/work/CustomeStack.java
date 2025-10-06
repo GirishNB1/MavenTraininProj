d1438abbd0a9d10945aef5794e2d8671061f1a5d
GirishNB1 <87060319+GirishNB1@users.noreply.github.com>
2025-10-06 14:30:30 +0530

    Stack

---
+++ src/main/java/com/work/CustomeStack.java
@@ -0,0 +1,33 @@
+package com.work;
+
+import java.util.Stack;
+
+public class CustomeStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    CStack<String> stk=new CStack();
+    stk.add("manu");
+    stk.add("Guru");
+    System.out.println(stk.peek());
+	}
+
+}
+
+
+class CStack<T>{
+	int top;
+	Object[] arr;
+	 CStack(){
+		top=-1;
+		this.arr=new Object[10];
+	}
+	 
+   public void  add(T data) {
+	   arr[++top]=data;
+   }
+   
+   public T peek() {
+	   return (T)arr[top];
+   }
+}
